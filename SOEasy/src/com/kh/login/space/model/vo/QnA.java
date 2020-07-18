fbcf6422f0484af1ee250d07d5a040e5375ceea4
beom <beom@DESKTOP-4P8F1ND>
2020-07-19 04:56:56 +0900

    review, qna vo 수정. 전체 공간정보 조회 메소드 수정

--- SOEasy/src/com/kh/login/space/model/vo/QnA.java
+++ SOEasy/src/com/kh/login/space/model/vo/QnA.java
@@ -4,7 +4,9 @@ public class QnA implements java.io.Serializable {
 
 	private int qNo;			//질문번호
 	private int qMemNo;			//문의자번호
+	private String qMemNick;	//문의자 닉네임
 	private int rMemNo;			//답변자번호
+	private String rMemNick;	//답변자 닉네임
 	private String qEnrollDate;	//작성일자
 	private String qContent;	//질문내용
 	private String rContent;	//답변내용
@@ -15,13 +17,15 @@ public class QnA implements java.io.Serializable {
 	public QnA() {
 		
 	}
-	
-	public QnA(int qNo, int qMemNo, int rMemNo, String qEnrollDate, String qContent, String rContent,
-			String rEnrollDate, int qKind, int spaceNo) {
+
+	public QnA(int qNo, int qMemNo, String qMemNick, int rMemNo, String rMemNick, String qEnrollDate, String qContent,
+			String rContent, String rEnrollDate, int qKind, int spaceNo) {
 		super();
 		this.qNo = qNo;
 		this.qMemNo = qMemNo;
+		this.qMemNick = qMemNick;
 		this.rMemNo = rMemNo;
+		this.rMemNick = rMemNick;
 		this.qEnrollDate = qEnrollDate;
 		this.qContent = qContent;
 		this.rContent = rContent;
@@ -46,12 +50,28 @@ public class QnA implements java.io.Serializable {
 		this.qMemNo = qMemNo;
 	}
 
+	public String getqMemNick() {
+		return qMemNick;
+	}
+
+	public void setqMemNick(String qMemNick) {
+		this.qMemNick = qMemNick;
+	}
+
 	public int getrMemNo() {
 		return rMemNo;
 	}
 
 	public void setrMemNo(int rMemNo) {
 		this.rMemNo = rMemNo;
+	}
+
+	public String getrMemNick() {
+		return rMemNick;
+	}
+
+	public void setrMemNick(String rMemNick) {
+		this.rMemNick = rMemNick;
 	}
 
 	public String getqEnrollDate() {
@@ -104,9 +124,9 @@ public class QnA implements java.io.Serializable {
 
 	@Override
 	public String toString() {
-		return "QnA [qNo=" + qNo + ", qMemNo=" + qMemNo + ", rMemNo=" + rMemNo + ", qEnrollDate=" + qEnrollDate
-				+ ", qContent=" + qContent + ", rContent=" + rContent + ", rEnrollDate=" + rEnrollDate + ", qKind="
-				+ qKind + ", spaceNo=" + spaceNo + "]";
+		return "QnA [qNo=" + qNo + ", qMemNo=" + qMemNo + ", qMemNick=" + qMemNick + ", rMemNo=" + rMemNo
+				+ ", rMemNick=" + rMemNick + ", qEnrollDate=" + qEnrollDate + ", qContent=" + qContent + ", rContent="
+				+ rContent + ", rEnrollDate=" + rEnrollDate + ", qKind=" + qKind + ", spaceNo=" + spaceNo + "]";
 	}
 	
 }
