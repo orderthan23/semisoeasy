ffb90a2c1b9d4c15a95530dc8ab4b762d2955cd3
kimin <kimin@DESKTOP-NVTK22F>
2020-07-22 15:39:36 +0900

    새 서블릿 위한 작업중^^

---
+++ SOEasy/src/com/kh/login/space/controller/IntoReservationServlet.java
@@ -0,0 +1,59 @@
+package com.kh.login.space.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.kh.login.space.model.service.SpaceService;
+
+/**
+ * Servlet implementation class IntoReservationServlet
+ */
+@WebServlet("/intoReservation")
+public class IntoReservationServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IntoReservationServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int sNo = Integer.parseInt(request.getParameter("spaceNo"));
+		System.out.println("너 값넘어왔냐?"+sNo);
+		
+		ArrayList<HashMap<String,Object>> list = new SpaceService().selectOneSpaceInfo(sNo);
+		
+		String page = "";
+		if(list != null) {
+			page ="/views/space/spaceReser.jsp";
+			request.setAttribute("introList", list);
+			request.getRequestDispatcher(page).forward(request, response);
+		} else {
+			page = "views/common/errorPage.jsp";
+			request.setAttribute("msg", "상세보기 실패");
+			request.getRequestDispatcher(page).forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
