1209375a8922c132e2f0a1b7c823985a779d3b9c
kimin <kimin@DESKTOP-NVTK22F>
2020-07-26 20:58:22 +0900

    parent key not found 이거 어떡해요..? 오라클쪽 에러라 그거 계속 구글링하고 db 보고 하느라 이클립스상에서 커밋할건 많이 없어요

--- SOEasy/src/com/kh/login/space/controller/InsertReviewServlet.java
+++ SOEasy/src/com/kh/login/space/controller/InsertReviewServlet.java
@@ -43,12 +43,13 @@ public class InsertReviewServlet extends HttpServlet {
 		
 		//reviewNo는 시퀀스로 주기
 //		int spaceNo = si.getSpaceNo();
-		int memberNo = loginUser.getMemberNo(); //로그인 안되있음 에러
+		int memberNo = loginUser.getMemberNo();
 		int spaceNo = 4;
 		String reviewContent = request.getParameter("reviewContent");
 		int rPoint = Integer.parseInt(request.getParameter("rPoint"));
 		String enrollDate = "SYSDATE";
 		//reservNo 는 시퀀스
+		
 		
 		Review requestMember = new Review();
 		requestMember.setSpaceNo(spaceNo);
